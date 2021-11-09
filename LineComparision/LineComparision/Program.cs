20745b0159293f9458a69239af81d20c8788cafe
asc303 <chauhansam94@gmail.com>
2021-11-09 22:58:56 +0530

    Line Comparision is created

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Line Comparison Computation Program");
+            LineCalculating lineOne = new LineCalculating(8.0, 6.0, 6.0, 8.0);
+            double lengthOne = lineOne.LengthCalculate();
+            Console.WriteLine("The length of the first line is {0}", lengthOne);
+            LineCalculating lineTwo = new LineCalculating(6.0, 7.0, 5.0, 9.0);
+            double lengthTwo = lineTwo.LengthCalculate();
+            Console.WriteLine("The length of the second line is {0}", lengthTwo);
+            lineTwo.Check(lengthOne, lengthTwo);
+        }
+    }
+}
+
+
